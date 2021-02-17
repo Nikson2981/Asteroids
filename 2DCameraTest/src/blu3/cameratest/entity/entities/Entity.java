6130c453c4600afe0c2a162ab6761839d2418a31
blu3 <67823680+blu3-65b@users.noreply.github.com>
2021-02-17 11:00:13 -0800

    Update Entity.java
    rofl

--- 2DCameraTest/src/blu3/cameratest/entity/entities/Entity.java
+++ 2DCameraTest/src/blu3/cameratest/entity/entities/Entity.java
@@ -5,14 +5,36 @@ public class Entity {
     private final String name;
     private final int defaultHealth;
     private int health;
-    private boolean isAlive = true;
+    private boolean isAlive, isInvulnerable;
     private float posX, posY, motionX, motionY;
 
+    //-----------------------------------------------------------------------
+    // This is the constructor class for all entities. 
+    // A few things here will only be used a few times, but still need them
+    // nonetheless. While this may seem easy to cheat, I'll get good stuff 
+    // going server-side whenever I get that running.
+    //
+    // Most methods here are fairly self-explanatory, hopefully being named
+    // well enough to be easily understood at first glance.
+    //
+    //-----------------------------------------------------------------------
     public Entity(String name, int defaultHealth, float startPosX, float startPosY) {
+        
+        if (name == null || defaultHealth == null || startPosX == null || startPosY == null) {
+            RuntimeException up = new RuntimeException("Entity parameters cannot be null");
+            throw up;
+        }
+        
         this.name = name;
         this.defaultHealth = defaultHealth;
         this.posX = startPosX;
         this.posY = startPosY;
+        this.isAlive = true;
+        this.isInvulnerable = false;
+    }
+    
+    public void tick() {
+        // This must be overridden for every entity other than the player. How would you give it an AI otherwise?
     }
 
     public String getName() {
@@ -62,6 +84,10 @@ public class Entity {
     public float getPosY() {
         return posY;
     }
+    
+    public boolean isInvulnerable() {
+        return this.isInvulnerable;
+    }
 
     public void setMotionX(float motionX) {
         this.motionX = motionX;
@@ -77,5 +103,9 @@ public class Entity {
 
     public void setPosY(float posY) {
         this.posY = posY;
+    }
+    
+    public void setInvulnerable(boolean invulnerable) {
+        this.isInvulnerable = invulnerable;
     }
 }
