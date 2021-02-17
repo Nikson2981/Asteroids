633cd75e374abde9d7b098eb63e414b7044e6c8d
blu3 <67823680+blu3-65b@users.noreply.github.com>
2021-02-17 10:09:31 -0800

    Update Camera.java

--- 2DCameraTest/src/blu3/cameratest/renderer/Camera.java
+++ 2DCameraTest/src/blu3/cameratest/renderer/Camera.java
@@ -1,8 +1,19 @@
 package blu3.cameratest.renderer;
 
+//-------------------------------------------
+// Responsible for telling things wether or not to draw. 
+// If something isn't on screen, it won't be told to draw.
+// Mostly just for adding offsets to existing things
+// so we actually have the ability to move around.
+//-------------------------------------------
 public class Camera {
+    
+    // dont instantiate this i stg
+    private Camera() {
+        
+    }
 
-    private static long offsetX, offsetY;
+    private static long offsetX = 0, offsetY = 0;
 
     public static long getOffsetX() {
         return offsetX;
