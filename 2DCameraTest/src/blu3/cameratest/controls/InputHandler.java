86b502fea2759a3aba75bf3fc277b5ddb2864acb
blu3 <67823680+blu3-65b@users.noreply.github.com>
2021-02-17 10:31:40 -0800

    Update InputHandler.java
    cleaned, added comments

--- 2DCameraTest/src/blu3/cameratest/controls/InputHandler.java
+++ 2DCameraTest/src/blu3/cameratest/controls/InputHandler.java
@@ -3,6 +3,12 @@ package blu3.cameratest.controls;
 import java.awt.event.*;
 import java.util.Arrays;
 
+	//------------------------------------------------------------------------------
+	// This is a terribly oversized way of handling input, most of these methods 
+	// do absolutely nothing, but are required to be there for some reason.
+	// TODO: MAJOR refactoring and cleaning
+	//------------------------------------------------------------------------------
+
 public class InputHandler implements KeyListener, FocusListener, MouseListener, MouseMotionListener {
 
 	public boolean[] key = new boolean[68836];
@@ -12,59 +18,54 @@ public class InputHandler implements KeyListener, FocusListener, MouseListener, 
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
+		// do nothing
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		MouseX = e.getX();
 		MouseY = e.getY();
-		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		// do nothing
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
 		mouseDown = true;
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
 		mouseDown = false;
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		// do nothing
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		// do nothing
 	}
 
 	@Override
 	public void focusGained(FocusEvent e) {
-		
+		// TOOD: autoresume setting
 	}
 
 	@Override
 	public void focusLost(FocusEvent e) {
 		Arrays.fill(key, false);
+		// TODO: pause
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		// TODO: something to do with typing rofl
 	}
 
 	@Override
@@ -73,16 +74,13 @@ public class InputHandler implements KeyListener, FocusListener, MouseListener, 
 		if (keyCode > 0 && keyCode < key.length) {
 			key[keyCode] = true;
 		}
-		
 	}
-
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 		if (keyCode > 0 && keyCode < key.length) {
 			key[keyCode] = false;
 		}
-		
 	}
-
 }
