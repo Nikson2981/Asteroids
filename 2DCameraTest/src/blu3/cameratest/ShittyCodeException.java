6584d60a7250b2b3751746dfc9964c38400e883c
blu3 <67823680+blu3-65b@users.noreply.github.com>
2021-02-17 11:07:06 -0800

    Update ShittyCodeException.java

--- 2DCameraTest/src/blu3/cameratest/ShittyCodeException.java
+++ 2DCameraTest/src/blu3/cameratest/ShittyCodeException.java
@@ -1,1 +1,18 @@
+package blu3.cameratest;
 
+
+// There is no way this just works on the first try
+// this was just written by directly editing the github page from google chrome on my phone
+
+public class ShittyCodeException extends RuntimeException {
+    public ShittyCodeException(String text) {
+        super(text);
+    }
+   
+    public void post() {
+        Logger.ERROR("[FATAL] ShittyCodeException thrown! Shutting down...");
+        this.printStackTrace();
+        System.exit(1);
+        throw this;
+    }
+}
